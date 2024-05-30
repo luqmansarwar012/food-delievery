d12adb8f80796cda19d9bf49c809d59d42c62a49
Raja Luqman Sarwar <luqmanrajput012@gmail.com>
2024-05-30 04:50:37 -0700

    feat(project): Setup react project for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar/Navbar.jsx"
+import './index.css'
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
