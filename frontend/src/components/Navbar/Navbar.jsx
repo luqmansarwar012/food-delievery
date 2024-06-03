00e0dfed07a0546f6f1ec136c7114df2fd6c511d
Raja Luqman Sarwar <luqmanrajput012@gmail.com>
2024-06-03 01:39:50 -0700

    feat(Cart Page): Create cart details secion on cart page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = ({ setShowLogin }) => {
     const [menu, setMenu] = useState('home')
     return (
         <div className='navbar'>
-            <img src={assets.logo} alt="" className='logo' />
+            <Link to="/"><img src={assets.logo} alt="" className='logo' /></Link>
             <ul className='navbar-menu'>
-                <Link onClick={() => { setMenu("home") }} className={menu === "home" ? "active" : ""}>Home</Link>
+                <Link onClick={() => { setMenu("home") }} className={menu === "home" ? "active" : ""} to='/'>Home</Link>
                 <a href='#explore-menu' onClick={() => { setMenu("menu") }} className={menu === "menu" ? "active" : ""}>Menu</a>
                 <a href='#app-download' onClick={() => { setMenu("mobile-app") }} className={menu === "mobile-app" ? "active" : ""}>Mobile app</a>
                 <a href='#footer' onClick={() => { setMenu("contact-us") }} className={menu === "contact-us" ? "active" : ""}>Contact us</a>
@@ -17,7 +17,7 @@ const Navbar = ({ setShowLogin }) => {
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="" />
                 <div className="navbar-search-icon">
-                    <img src={assets.basket_icon} alt="" />
+                    <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                     <div className="dot"></div>
                 </div>
                 <button onClick={() => setShowLogin(true)}>Sign in</button>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -16,6 +16,54 @@ const Cart = () => {
                 </div>
                 <br />
                 <hr />
+                {food_list.map((item, index) => {
+                    if (cartItems[item._id] > 0) {
+                        return (
+                            <div key={index}>
+                                <div className='cart-items-title cart-items-item'>
+                                    <img src={item.image} alt="" />
+                                    <p>{item.name}</p>
+                                    <p>$ {item.price}</p>
+                                    <p>{cartItems[item._id]}</p>
+                                    <p>$ {item.price * cartItems[item._id]}</p>
+                                    <p className='cross' onClick={() => removeFromCart(item._id)}>x</p>
+                                </div>
+                                <hr />
+                            </div>
+                        )
+                    }
+                })}
+            </div>
+            <div className="cart-bottom">
+                <div className="cart-total">
+                    <h2>Cart Totals</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Subtotal</p>
+                            <p>{0}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delievery Fee</p>
+                            <p>{0}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Total</p>
+                            <p>{0}</p>
+                        </div>
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
+                </div>
+                <div className="cart-promocode">
+                    <div>
+                        <p>If you have a promo code, Enter it here!</p>
+                        <div className="cart-promocode-input">
+                            <input type="text" name="promocode" id="" placeholder='Promo code' />
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
