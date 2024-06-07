5c8f3562b0b77750343a1bc2f2b70363baeb219b
Raja Luqman Sarwar <luqmanrajput012@gmail.com>
2024-06-07 04:43:22 -0700

    feat(frontend-myorders: create structure for myorders page

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react'
 import './MyOrders.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios'
+import { assets } from '../../assets/assets'
 const MyOrders = () => {
     const { url, token } = useContext(StoreContext)
     const [data, setData] = useState([])
@@ -16,7 +17,26 @@ const MyOrders = () => {
         }
     }, [token])
     return (
-        <div>MyOrders</div>
+        <div className='my-orders'>
+            <h2>My Orders</h2>
+            <div className="container">
+                {
+                    data.map((order, index) => {
+                        return (<div key={index}>
+                            <img src={assets.parcel_icon} alt="" />
+                            <p>{order.items.map((item, key) => {
+                                if (index === order.items.length - 1) {
+                                    return item.name + ' x ' + item.quantity
+                                }
+                                else {
+                                    return item.name + ' x ' + item.quantity + ", "
+                                }
+                            })}</p>
+                        </div>)
+                    })
+                }
+            </div>
+        </div>
     )
 }
 
