1a89c4fb10b37bf0b298c43f1d358976fd413ac1
Raja Luqman Sarwar <luqmanrajput012@gmail.com>
2024-06-07 04:05:57 -0700

    feat(backend-order): Add userOrders route to fetch user's orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -69,4 +69,15 @@ const verifyOrder = async (req, res) => {
     res.json({ success: false, message: "Something went wrong!" });
   }
 };
-export { placeOrder, verifyOrder };
+
+// user orders for frontend
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Something went wrong" });
+  }
+};
+export { placeOrder, verifyOrder, userOrders };
